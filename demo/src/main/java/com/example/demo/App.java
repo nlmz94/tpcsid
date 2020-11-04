c757d9be2c4ceacc9b9866ac5b453fbf2a53da52
nlmz94 <46894034+nlmz94@users.noreply.github.com>
2020-11-04 17:10:05 +0100

    test3
    test33

---
+++ demo/src/main/java/com/example/demo/App.java
@@ -0,0 +1,8 @@
+package com.example.demo;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        //SpringApplication.run(DemoApplication.class, args);
+    }
+}
