fa9a9f46d11ddbfa244713cd1403c0ea8c0627bc
nlmz94 <46894034+nlmz94@users.noreply.github.com>
2020-11-05 11:49:31 +0100

    commit_onlyweebs
    salutation jeune entrepreneur

---
+++ OnlyWeebs/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { SigninComponent } from './signin/signin.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SigninComponent
+  ],
+  imports: [
+    BrowserModule,
+    NgbModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
